7f152c7deaf97972a337478ad8be6940e6deb41e
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2024-09-25 19:26:06 +0530

    Create 1. Array Declaration.cpp

---
+++ Array Representations/1. Array Declaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Declaration
+    int A[5];
+
+    //Declaration and Initialisation
+    int B[5] = {1,2,3,4,5};
+    int C[10] = {2,4,6};        //Rest set to zero
+    int D[5] = {0};             //All values set to zero
+    int E[] = {1,2,3,4,5,6};    //Size set to 6
+
+    return 0;
+}
