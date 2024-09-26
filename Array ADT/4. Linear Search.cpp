673c6f6d992b73b972f34b2a923e3c3cbcbbffd7
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2024-09-26 19:40:35 +0530

    Update 4. Linear Search.cpp

--- Array ADT/4. Linear Search.cpp
+++ Array ADT/4. Linear Search.cpp
@@ -10,11 +10,7 @@ class Array{
     public:
         void initialise();              //Time - O(n) | Space - O(n)
         void display();                 //Time - O(n) | Space - O(1)
-        void append(int x);             //Time - O(1) | Space - O(1)
-        void insert(int index, int x);  //Time - O(n) | Space - O(1)
-        bool isValid(int index);        //Time - O(1) | Space - O(1)
-        bool isFull();                  //Time - O(1) | Space - O(1)
-        void deleteIndex(int index);    //Time - O(n) | Space - O(1)
+
         int linearSearch(int key);      //Time - O(n) | Space - O(1)
 };
 
@@ -45,47 +41,6 @@ void Array :: display(){
         cout<<endl;
 }
 
-void Array :: append(int x){
-        if(!isFull()){
-            A[length] = x;
-            length++;
-        }
-}
-
-bool Array :: isValid(int index){
-    if(index >= 0 && index <= length){
-        return 1;
-    }
-    cout<<"Error: Invalid index"<<endl;
-    return 0;
-}
-
-bool Array :: isFull(){
-    if(length>=size){
-        cout<<"Error: Size full"<<endl;
-        return 1;
-    }
-    return 0;
-}
-
-void Array :: insert(int index, int x){
-    if(isValid(index) && !isFull()){
-        for(int i = length; i>index; i--)
-            A[i] = A[i-1];
-        A[index] = x;
-        length++;
-    }
-}
-
-void Array :: deleteIndex(int index){
-    if(isValid(index)){
-        for(int i = index; i<length-1; i++){
-            A[i] = A[i+1];
-        }
-        length--;
-    }
-}
-
 int Array :: linearSearch(int key){         //Returns index of key;
     for(int i=0;i<length;i++){
         if(key == A[i]){
@@ -102,13 +57,7 @@ int main()
     Array arr;
     arr.initialise();
     arr.display();
-    arr.append(15);
-    arr.insert(4,20);
-    arr.display();
-    arr.deleteIndex(6);
-    arr.display();
     cout<<"Linear Search for key '5' at index: "<<arr.linearSearch(5)<<endl;
-    arr.display();
     
     return 0;
 }
