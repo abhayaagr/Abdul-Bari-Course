99e5798c86744c799620fd69ca9df4015f7c6dde
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2024-09-26 15:41:42 +0530

    Create 1. Array ADT.cpp

---
+++ Array ADT/1. Array ADT.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Array{
+    private:
+        int *A;
+        int size;
+        int length;
+        
+    public:
+        void initialise();
+        void display();
+};
+
+void Array :: initialise(){
+        cout<<"Enter size of array: ";
+        cin>>size;
+        A = new int[size];
+            
+        cout<<"Enter length of numbers: ";
+        cin>>length;
+            
+        cout<<"Enter all elements:"<<endl;
+        for(int i = 0;i<length;i++){
+            cin>>A[i];
+        }
+}
+
+void Array :: display(){
+        cout<<"Displaying all the elements of array:"<<endl;
+        for(int i = 0;i<length;i++){
+           cout<<A[i]<<" ";
+        }
+}
+
+int main()
+{
+    Array arr;
+    arr.initialise();
+    arr.display();
+    
+    return 0;
+}
