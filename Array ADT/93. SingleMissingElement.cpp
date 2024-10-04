0a88c5f200b2181ea44fc5e0c21a98379aaed7df
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2024-10-04 22:47:43 +0530

    Create 93. SingleMissingElement.cpp

---
+++ Array ADT/93. SingleMissingElement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int singleMissing(int arr[], int n){
+    if(n==0)    return 0;
+    int diff = arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i] - i != diff){
+            return i+diff;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {5,6,8,9,10,11,12,13,14};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<singleMissing(arr,n);
+    
+    return 0;
+}
