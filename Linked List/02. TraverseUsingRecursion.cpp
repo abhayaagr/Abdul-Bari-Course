f180566d0d5976927f4515d0bfb308dc0c914ece
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2025-01-18 19:49:09 +0530

    Create 02. TraverseUsingRecursion.cpp

---
+++ Linked List/02. TraverseUsingRecursion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+}*head = NULL;
+
+void create(int arr[], int n){
+     struct Node *temp, *last;
+     head = new Node;
+     head->data = arr[0];
+     head->next = NULL;
+     last = head;
+     for(int i=1;i<n;i++){
+         temp = new Node;
+         temp->data = arr[i];
+         temp->next = NULL;
+         last->next = temp;
+         last = temp;
+     }
+}
+
+void display(struct Node *p){       //Traversal using Loop
+    while(p != NULL){
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+void Rdisplay(struct Node *p){      //Traversal using Recursion
+    if(p != NULL){
+        // cout<<p->data<<" ";     //Prints forward
+        Rdisplay(p->next);
+        cout<<p->data<<" ";     //Prints backward
+    }
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    create(arr, 5);
+    Rdisplay(head);
+    return 0;
+}
