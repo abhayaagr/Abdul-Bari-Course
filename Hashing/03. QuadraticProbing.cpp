338f3ef94ae351e90e460d9b8e626a93050f9341
Abhaya Kumar Agrawal <117733470+abhayaagr@users.noreply.github.com>
2025-02-17 16:43:37 +0530

    Create 03. QuadraticProbing.cpp

---
+++ Hashing/03. QuadraticProbing.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define size 10
+using namespace std;
+
+int hashFn(int key){
+    return key%size;
+}
+
+int probe(int H[], int key){
+    int index = hashFn(key);
+    int i = 0;
+    while(H[(index+i*i)%size] != 0)
+        i++;
+    return (index+i*i)%size;
+}
+
+void insert(int H[], int key){
+    int index = probe(H, key);
+    H[index] = key;
+}
+
+int search(int H[], int key){
+    int index = hashFn(key);
+    int i = 0;
+    while(H[(index+i*i)%size] != key){
+        if(H[(index+i*i)%size] == 0)
+            return -1;
+        i++;
+    }
+    return (index+i*i)%size;
+}
+
+int main() {
+    int H[size] = {0};
+
+    insert(H, 5);
+    insert(H, 15);
+    insert(H, 52);
+    insert(H, 23);
+    insert(H, 2);
+    int key = 2;
+    cout<<search(H, key)<<endl;
+    key = 52;
+    cout<<search(H, key)<<endl;
+    key = 3;
+    cout<<search(H, key)<<endl;
+    return 0;
+}
